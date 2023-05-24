b779849f9f871f5233f990d91d084fddb3191df0
Julia Mistieri <julia.mistieri02@gmail.com>
2023-05-24 17:32:51 -0400

    commit
    aula dia 24/05

---
+++ exercicio_03.js
@@ -0,0 +1,36 @@
+class NerdIF {
+  constructor(estudante, cosplay, nota_cosplay) {
+    this.estudante = estudante;
+    this.cosplay = cosplay;
+    this.nota_cosplay = nota_cosplay;
+  }
+
+  retornarAtributos() {
+    try {
+      return this.atributos();
+  } catch (erro){
+      console.log(erro.stack)
+  }
+  }
+  
+  atributos() {
+    
+      if (this.estudante != "" && this.cosplay != "" && this.nota_cosplay != ""){
+        return {
+          estudante: this.estudante,
+          cosplay: this.cosplay,
+          nota_cosplay: this.nota_cosplay
+        };
+      }else {
+          throw new Error("Está faltando estudante, cosplay e nota cosplay :)")
+      }
+    
+  }
+}
+
+const aluno = new NerdIF("", "Homem-Aranha", 9.5);
+const atributos = aluno.retornarAtributos();
+
+//console.log(atributos.estudante); 
+//console.log(atributos.cosplay);   
+//console.log(atributos.nota_cosplay);
